a77fc05fd8af366fa8dd21aa4c10d29068ee9d8d
Roman A <dev@rman.dev>
2025-09-24 22:45:19 +0100

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  output: "static",
   integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: { plugins: [tailwindcss()] },
 });
