0445ac901ebbae06a7f6fa845aa244a65a8658a6
Roman <dev@rman.dev>
2026-05-17 01:39:36 +0100

    chore: format

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -218,7 +218,7 @@ const App = () => {
                     strokeWidth="1"
                   />
                 </svg>
-            </div>
+              </div>
             </div>
           </div>
         </section>
